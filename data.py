e7d143e98ae1659330fd62138ada220cc681cdc8
NurikNK <Nur_09_08_00@mail.ru>
2021-10-04 12:36:06 +0600

    create autch app

---
+++ data.py
@@ -0,0 +1,20 @@
+USERS = [
+    {
+        "fullname": "Allex Zolo",
+        "username": "zolo123",
+        "password": "super_pass2021",
+        "logged_in": False
+    },
+    {
+        "fullname": "AJ Meyani",
+        "username": "aj-cool",
+        "password": "aj=boi2021",
+        "logged_in": False
+    },
+    {
+        "fullname": "Shanon Ruden",
+        "username": "Shanon-lady",
+        "password": "shaly2021",
+        "logged_in": False
+    }
+]
